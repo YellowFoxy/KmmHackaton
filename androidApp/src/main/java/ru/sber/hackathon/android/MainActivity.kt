ff1d2be9ed46c75edaa01ba821aeaa8563c70680
Mikhail Tsvetkov <MsTsvetkov@sberbank.ru>
2022-12-05 13:09:04 +0300

    KtorClient

--- androidApp/src/main/java/ru/sber/hackathon/android/MainActivity.kt
+++ androidApp/src/main/java/ru/sber/hackathon/android/MainActivity.kt
@@ -6,11 +6,32 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.launch
 import ru.sber.hackathon.Greeting
+import ru.sber.hackathon.network.KtorClient
 
 class MainActivity : ComponentActivity() {
+    // override fun onCreate(savedInstanceState: Bundle?) {
+    //     super.onCreate(savedInstanceState)
+    //     setContent {
+    //         MyApplicationTheme {
+    //             Surface(
+    //                 modifier = Modifier.fillMaxSize(),
+    //                 color = MaterialTheme.colors.background
+    //             ) {
+    //                 GreetingView(Greeting().greet())
+    //             }
+    //         }
+    //     }
+    // }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -19,11 +40,23 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    val scope = rememberCoroutineScope()
+                    var text by remember { mutableStateOf("Loading") }
+                    LaunchedEffect(true) {
+                        scope.launch {
+                            text = try {
+                                KtorClient().getData()
+                            } catch (e: Exception) {
+                                e.localizedMessage ?: "error"
+                            }
+                        }
+                    }
+                   GreetingView(text)
                 }
             }
         }
     }
+
 }
 
 @Composable
