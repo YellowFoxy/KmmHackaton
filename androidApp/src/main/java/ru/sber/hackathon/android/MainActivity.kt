d84a2756e1fc99136bdfa0f5ddfd6d8e33d77e12
Малышев Александр Петрович <Malyshev.A.Pet@sberbank.ru>
2022-12-05 14:30:45 +0300

    Auth screens and navigation (demo)

--- androidApp/src/main/java/ru/sber/hackathon/android/MainActivity.kt
+++ androidApp/src/main/java/ru/sber/hackathon/android/MainActivity.kt
@@ -1,52 +1,14 @@
 package ru.sber.hackathon.android
 
 import android.os.Bundle
-import android.widget.EditText
-import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import kotlinx.coroutines.launch
-import ru.sber.hackathon.Greeting
-import ru.sber.hackathon.network.KtorClient
 
 class MainActivity : ComponentActivity() {
-    // override fun onCreate(savedInstanceState: Bundle?) {
-    //     super.onCreate(savedInstanceState)
-    //     setContent {
-    //         MyApplicationTheme {
-    //             Surface(
-    //                 modifier = Modifier.fillMaxSize(),
-    //                 color = MaterialTheme.colors.background
-    //             ) {
-    //                 GreetingView(Greeting().greet())
-    //             }
-    //         }
-    //     }
-    // }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -58,21 +20,20 @@ class MainActivity : ComponentActivity() {
                 ) {
                     composable("auth") { AuthScreen(navController) }
                     composable("registration") { RegistrationScreen() }
-                    val scope = rememberCoroutineScope()
-                    var text by remember { mutableStateOf("Loading") }
-                    LaunchedEffect(true) {
-                        scope.launch {
-                            text = try {
-                                KtorClient().getData()
-                            } catch (e: Exception) {
-                                e.localizedMessage ?: "error"
-                            }
-                        }
-                    }
-                   GreetingView(text)
+                    //  val scope = rememberCoroutineScope()
+                    //  var text by remember { mutableStateOf("Loading") }
+                    //  LaunchedEffect(true) {
+                    //      scope.launch {
+                    //          text = try {
+                    //              KtorClient().getData()
+                    //          } catch (e: Exception) {
+                    //              e.localizedMessage ?: "error"
+                    //          }
+                    //      }
+                    //  }
+                    // GreetingView(text)
                 }
             }
         }
     }
-
 }
