575d1d7b758dd5ee5def8f3c1b364a1a7ffc0c3e
Малышев Александр Петрович <Malyshev.A.Pet@sberbank.ru>
2022-12-06 13:04:37 +0300

    Fix aligment

--- androidApp/src/main/java/ru/sber/hackathon/view/auth/PasswordRestoreScreen.kt
+++ androidApp/src/main/java/ru/sber/hackathon/view/auth/PasswordRestoreScreen.kt
@@ -76,7 +76,8 @@ fun PasswordRestoreScreen() {
         Text(
             modifier = Modifier.padding(bottom = 26.dp),
             fontSize = 12.sp,
-            text = stringResource(R.string.passwordRestoreInstruction))
+            text = stringResource(R.string.passwordRestoreInstruction)
+        )
         Button(
             modifier = Modifier
                 .fillMaxWidth()
@@ -104,44 +105,44 @@ fun PasswordRestoreScreen() {
                 text = stringResource(R.string.restorePassword)
             )
         }
-        SnackbarHost(
-            modifier = Modifier
-                .fillMaxSize(),
-            hostState = snackbarHostState,
-            snackbar = { snackbarData: SnackbarData ->
-                Column(
+    }
+    SnackbarHost(
+        modifier = Modifier
+            .fillMaxSize(),
+        hostState = snackbarHostState,
+        snackbar = { snackbarData: SnackbarData ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Bottom
+            ) {
+                Row(
                     modifier = Modifier
-                        .fillMaxSize(),
-                    verticalArrangement = Arrangement.Bottom
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 24.dp)
+                        .background(
+                            shape = RoundedCornerShape(12.dp),
+                            color = Color(0xFF31373B)
+                        ),
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Row(
+                    Icon(
+                        painterResource(id = R.drawable.snack_icon),
+                        contentDescription = null,
+                        tint = Color(0xFFE35502),
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(horizontal = 16.dp, vertical = 24.dp)
-                            .background(
-                                shape = RoundedCornerShape(12.dp),
-                                color = Color(0xFF31373B)
-                            ),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Icon(
-                            painterResource(id = R.drawable.snack_icon),
-                            contentDescription = null,
-                            tint = Color(0xFFE35502),
-                            modifier = Modifier
-                                .padding(horizontal = 17.dp, vertical = 12.dp),
-                        )
-                        Text(
-                            modifier = Modifier
-                                .padding(vertical = 15.dp),
-                            text = snackbarData.message,
-                            color = Color.White
-                        )
-                    }
+                            .padding(horizontal = 17.dp, vertical = 12.dp),
+                    )
+                    Text(
+                        modifier = Modifier
+                            .padding(vertical = 15.dp),
+                        text = snackbarData.message,
+                        color = Color.White
+                    )
                 }
             }
-        )
-    }
+        }
+    )
 }
 
 @Preview
